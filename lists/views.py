00c45551d886aa3956879a60341c4cece338e1d2
Matt Smrke <matthew.smrke@empire.ca>
2016-06-03 14:23:14 -0400

    refactors to use templates

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do Lists</title></html>')
+    return render(request, 'home.html')
+
