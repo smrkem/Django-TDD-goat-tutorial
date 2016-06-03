75ee39fe5a65e2392eda5e9fee08a05865400164
Matt Smrke <matthew.smrke@empire.ca>
2016-06-03 16:26:46 -0400

    initial Items model

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,28 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first to-do item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second to-do item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first to-do item')
+        self.assertEqual(second_saved_item.text, 'The second to-do item')
 
 
 class HomePageTest(TestCase):
