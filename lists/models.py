b0388921688c156041c86597916188468ed65850
Matt Smrke <matthew.smrke@empire.ca>
2016-06-04 15:38:37 -0400

    mutliple lists

--- lists/models.py
+++ lists/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default='')
-    
+
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -41,14 +41,36 @@ class HomePageTest(TestCase):
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
-        request.method = 'post'
+        request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'},
-            request=request
-        )
+
+        home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
 
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('1. item 1', response.content.decode())
+        self.assertIn('2. item 2', response.content.decode())
