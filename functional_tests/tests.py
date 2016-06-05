305d9969c84e5574e4d756f427e098fdfd5a1a58
Matt Smrke <matthew.smrke@empire.ca>
2016-06-05 16:12:28 -0400

    tests staticfiles

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -103,4 +104,6 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=5
         )
+        # self.assertAlmostEqual(inputbox.size['width'], 220, delta=10)
+
 
