d713bc52c2fed824e1f40b4d8d3e8594b0e7def4
Matt Smrke <matthew.smrke@empire.ca>
2016-06-05 15:46:27 -0400

    initial FT for layout and style

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -82,3 +82,25 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Happy
 
+    def test_layout_and_styling(self):
+        # Jenn goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # And sees it is nicely styled
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the list page is also nicely styled
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
