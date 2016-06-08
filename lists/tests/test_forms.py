bad2b969226aca54c33bce3b1e61cb952d1b3dd1
Matt Smrke <matthew.smrke@empire.ca>
2016-06-07 21:35:43 -0400

    uses and tests existing list form

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -46,3 +46,8 @@ class ExistingItemFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'test'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.first())
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 from django.utils.html import escape
 
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm, ExistingListItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+)
 from unittest import skip
 
 
@@ -121,10 +123,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/{}/'.format(list_.id))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_for_duplicate_item_show_error_on_page(self):
         list_ = List.objects.create()
         item1 = Item.objects.create(list=list_, text='same text')
@@ -132,7 +133,7 @@ class ListViewTest(TestCase):
             'text': 'same text'
         })
 
-        expected_error = escape("List item already exists in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.count(), 1)
