99bb9b8303ed0ea0190fe781d9492cdc306a9240
Matt Smrke <matthew.smrke@empire.ca>
2016-06-16 09:01:29 -0400

    missed this

--- lists/static/js/list.js
+++ lists/static/js/list.js
@@ -1,3 +1,6 @@
-$('input').on('keypress', function() {
-    $('.has-error').hide();
+jQuery(document).ready(function ($) {
+    $('input').on('keypress', function() {
+        $('.has-error').hide();
+    });
+
 });
