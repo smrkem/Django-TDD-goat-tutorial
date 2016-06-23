c84dc70ff048e19cf8371d5eae6d7562a715c436
Matt Smrke <matthew.smrke@empire.ca>
2016-06-23 18:27:17 -0400

    passing tests for onlogin initial

--- accounts/static/js/accounts.js
+++ accounts/static/js/accounts.js
@@ -3,7 +3,16 @@ var initialize = function(navigator, user, token, urls) {
       navigator.id.request();
     });
 
-    navigator.id.watch({ loggedInUser: user });
+    var onLogin =
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function(assertion) {
+            $.post(
+                urls.login,
+                {assertion: assertion, csrfmiddlewaretoken: token}
+            );
+        }
+     });
 };
 
 window.Superlists = {
