ead55cb37ee9ed2495da8ea11b2cfc26e3ea3ff5
Matt Smrke <matthew.smrke@empire.ca>
2016-06-24 09:20:52 -0400

    js tests for persona login stuffs

--- accounts/static/js/accounts.js
+++ accounts/static/js/accounts.js
@@ -3,14 +3,17 @@ var initialize = function(navigator, user, token, urls) {
       navigator.id.request();
     });
 
-    var onLogin =
     navigator.id.watch({
         loggedInUser: user,
         onlogin: function(assertion) {
             $.post(
                 urls.login,
                 {assertion: assertion, csrfmiddlewaretoken: token}
-            );
+            ).done(function(){
+                window.location.reload();
+            }).fail(function() {
+                navigator.id.logout();
+            });
         },
         onlogout: function() {}
      });
